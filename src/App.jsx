952dda0d10cc2c83715d5a3f7669047f4f0dfc20
LuisMata <mata.ramirez.1iv14@gmail.com>
2023-01-25 12:55:54 -0600

    aboutPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import { Layout } from './comoponents/Layout'
 import { Home } from './comoponents/Home'
+import { About } from './comoponents/About'
 import './App.scss'
 
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout/>}>
           <Route index element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
         </Route>
       </Routes>
     </>
